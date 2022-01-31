8faf5b1128fee1938c79200be962274e24fa2223
SamuelVasquezGonzalez <samuelgonza2006@gmail.com>
2022-01-30 20:14:01 -0500

    Subida de prueba

---
+++ scripts/mostrar.js
@@ -0,0 +1,19 @@
+const botonMostrar = document.querySelector(".more");
+const cuerpoOculto = document.querySelector(".cuerpo-mas");
+
+botonMostrar.addEventListener("click", mostrar);
+
+function mostrar () {
+    cuerpoOculto.classList.toggle("oculto");
+    clase = cuerpoOculto.classList;
+    
+    botonMostrar.innerHTML = 'Ver Menos <i class="fas fa-arrow-up"></i>';
+    let icono = '<i class="fas fa-arrow-down"></i>';
+    while(clase[1] == "oculto"){
+        botonMostrar.innerHTML = `Ver Mas Servicios ${icono}`;
+        window.scroll(0, 0);
+        break;
+    }
+
+    window.scroll(0, 1500);
+}
